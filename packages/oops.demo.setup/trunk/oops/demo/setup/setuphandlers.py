6c70a89a2e9f96857e473a92cb0e603f7f830eab
fr.merlo@gmail.com <fr.merlo@gmail.com@d67fe582-f975-982c-faea-898842e22040>
2010-04-14 08:32:06 +0000

    first import of oops.demo.setup

---
+++ packages/oops.demo.setup/trunk/oops/demo/setup/setuphandlers.py
@@ -0,0 +1,36 @@
+import os
+from oops.demo import setup as CURR_DIR
+
+CONTENTS_TO_REMOVE = [
+    'news',
+    'events',
+    'Members',
+]
+
+ZEXP_TO_IMPORT = [
+    'book-1.zexp'
+]
+
+curr_dir = os.path.dirname(CURR_DIR.__file__)
+
+def removePloneContents(context):
+    if context.readDataFile('oops.demo.setup.setup_various.txt') is None: return
+    
+    portal = context.getSite()
+    
+    for content_id in CONTENTS_TO_REMOVE:
+        if content_id in portal.objectIds():
+            portal._delObject(content_id)
+            
+            
+def importDemoContents(context):
+    if context.readDataFile('oops.demo.setup.setup_various.txt') is None: return
+
+    portal = context.getSite()
+    
+    for zexp in ZEXP_TO_IMPORT:
+        try:
+            portal._importObjectFromFile(os.path.join(curr_dir, zexp))
+        except:
+            pass
+    
---
+++ packages/oops.demo.setup/trunk/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='oops.demo.setup',
+      version=version,
+      description="OOPS Demo Setup",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Francesco Merlo',
+      author_email='francesco.merlo@reflab.com',
+      url='https://online-offline-ps.googlecode.com/svn/packages/oops.demo.setup',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['oops', 'oops.demo'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'Plone',
+          'oops.staticdump',
+          'oops.dumpers',
+          'collective.book',
+          'oops.demo.dumptheme',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
