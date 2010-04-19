511054ee70bce24093733f8555c4c0ebfd38a792
fr.merlo@gmail.com <fr.merlo@gmail.com@d67fe582-f975-982c-faea-898842e22040>
2010-04-19 15:02:39 +0000

    Added javascripts for static site

---
+++ packages/oops.demo.dumptheme/trunk/oops/demo/dumptheme/skins/oops_demo_dumptheme_static/gears_config.js
@@ -0,0 +1,8 @@
+// GLOBAL VARIABLES FOR offline.js
+
+SITE_NAME = 'OOPS Demo'
+GEARS_MESSAGE = 'Install Gears to enable offline features'
+SITE_URL = $('base').attr('href')
+RETURN_URL = SITE_URL + 'offline.html'
+GEARS_ICON = SITE_URL + 'gears_icon.png'
+STORE_NAME = 'oops_demo_offline'
