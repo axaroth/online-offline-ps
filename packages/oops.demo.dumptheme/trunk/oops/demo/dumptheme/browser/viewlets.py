8f992f416d64a543cbd72660fb49a3b76522b483
fr.merlo@gmail.com <fr.merlo@gmail.com@d67fe582-f975-982c-faea-898842e22040>
2010-04-20 16:51:50 +0000

    Fixed global section and search

---
+++ packages/oops.demo.dumptheme/trunk/oops/demo/dumptheme/browser/viewlets.py
@@ -0,0 +1,18 @@
+from plone.app.layout.viewlets import common
+
+       
+class GlobalSectionsViewlet(common.GlobalSectionsViewlet):
+
+    def update(self):
+        super(GlobalSectionsViewlet, self).update()
+        self.selected_portal_tab = 'index_html'
+        import pdb; pdb.set_trace()
+        
+        # customized in order to enable the 'selected' class even w/o request
+        # for the static version
+        # XXX this is not the best way to fix this problem
+        for tab in self.portal_tabs:
+            if tab['id'] == 'index_html': continue
+            if self.context.absolute_url().startswith(tab['url']):
+                self.selected_portal_tab = tab['id']
+                break  
