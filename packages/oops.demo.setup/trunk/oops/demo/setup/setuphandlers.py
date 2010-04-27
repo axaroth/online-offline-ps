b055ce1769d64e6de53020bde2f324058e155ab9
antonio.tirabasso <antonio.tirabasso@d67fe582-f975-982c-faea-898842e22040>
2010-04-27 13:33:33 +0000

    added alice in wonderland sample and create dependencies between import steps

--- packages/oops.demo.setup/trunk/oops/demo/setup/setuphandlers.py
+++ packages/oops.demo.setup/trunk/oops/demo/setup/setuphandlers.py
@@ -5,10 +5,14 @@ CONTENTS_TO_REMOVE = [
     'news',
     'events',
     'Members',
+    'front-page',
 ]
 
 ZEXP_TO_IMPORT = [
-    'book-1.zexp'
+#    'book-1.zexp'
+    'zexp/front-page.zexp',
+    'zexp/alice-adventures-in-wonderland.zexp',
+    'zexp/through-the-looking-glass.zexp'
 ]
 
 curr_dir = os.path.dirname(CURR_DIR.__file__)
@@ -27,7 +31,6 @@ def importDemoContents(context):
     if context.readDataFile('oops.demo.setup.setup_demo.txt') is None: return
 
     portal = context.getSite()
-    
     for zexp in ZEXP_TO_IMPORT:
         try:
             portal._importObjectFromFile(os.path.join(curr_dir, zexp))
