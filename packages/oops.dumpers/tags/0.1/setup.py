6ad8cc4c5821d76dfde50195dfbd422463a7a071
fr.merlo@gmail.com <fr.merlo@gmail.com@d67fe582-f975-982c-faea-898842e22040>
2010-04-30 13:57:01 +0000

    Tagging 0.1

---
+++ packages/oops.dumpers/tags/0.1/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.1'
+
+setup(name='oops.dumpers',
+      version=version,
+      description="",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Reflab srl',
+      author_email='info@reflab.it',
+      url='http://code.google.com/p/online-offline-ps',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['oops'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
