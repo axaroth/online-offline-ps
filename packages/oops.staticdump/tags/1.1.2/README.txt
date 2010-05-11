47bff608e6da63736bc7a98793e7d8e9a442a154
antonio.tirabasso <antonio.tirabasso@d67fe582-f975-982c-faea-898842e22040>
2010-05-11 15:50:37 +0000

    Tagging 1.1.2

---
+++ packages/oops.staticdump/tags/1.1.2/README.txt
@@ -0,0 +1,10 @@
+Introduction
+============
+
+    ...
+
+Note
+====
+
+  Kupu must be configured to generate link with resolveUid. This generate
+  correct url for dumper code.
---
+++ packages/oops.staticdump/tags/1.1.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.1.2'
+
+setup(name='oops.staticdump',
+      version=version,
+      description="Create a static dump of a site containing collective.book",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+      keywords='',
+      author='Reflab srl',
+      author_email='info@reflab.it',
+      url='http://code.google.com/p/online-offline-ps',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['oops'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'collective.transmogrifier',
+          'plone.app.transmogrifier',
+          'BeautifulSoup',
+          'simplejson',
+      ],
+      entry_points=""" """,
+      )
