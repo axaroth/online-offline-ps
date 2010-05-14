defd8df5be29b25791ea491414cb0460449e0294
fr.merlo@gmail.com <fr.merlo@gmail.com@d67fe582-f975-982c-faea-898842e22040>
2010-05-14 16:38:32 +0000

    online search development

---
+++ javascripts/oops-gears/branches/online_search/gears_utils.js
@@ -0,0 +1,35 @@
+// DB utils
+/**
+ * For each row in the result set f will be called with the following
+ * parameters: row (map where the column names are the key) and rowIndex.
+ *
+ * @param {Object} db The database object
+ * @param {String} sql The SQL statement to execute
+ * @param {Array} args query params
+ * @param {Function} f Function to call for each row
+ */
+function forEachRow(db, sql, args, f) {
+  var rs = databaseServer.execute(sql, args);
+  try {
+    var rowIndex = 0;
+    var cols = rs.fieldCount();
+    var colNames = [];
+    for (var i = 0; i < cols; i++) {
+      colNames.push(rs.fieldName(i));
+    }
+
+    var rowMap;
+    while (rs.isValidRow()) {
+      rowMap = {};
+      for (var i = 0; i < cols; i++) {
+        rowMap[colNames[i]] = rs.field(i);
+      }
+      f.call(null, rowMap, rowIndex);
+      rs.next();
+      rowIndex++;
+    }
+  } finally {
+    rs.close();
+  }
+}
+
