68edad02494c8d5c0b0d2b680ab50e0b58a37e1b
fr.merlo@gmail.com <fr.merlo@gmail.com@d67fe582-f975-982c-faea-898842e22040>
2010-05-18 08:50:20 +0000

    Completed online search app

--- packages/oops.onlinesearch/trunk/oops/onlinesearch/__init__.py
+++ packages/oops.onlinesearch/trunk/oops/onlinesearch/__init__.py
@@ -7,26 +7,48 @@ from paste.request import parse_formvars
 class OOPSOnlineSearch(object):
 
     def __init__(self, database_path):
-        connection = sqlite3.connect(database_path)
-        self.cursor = connection.cursor()        
+        self.database_path = database_path
     
     def __call__(self, environ, start_response):
-        start_response('200 OK', [('content-type', 'application/json')])
         search_term = parse_formvars(environ).get('SearchTerm')
+        document_type = parse_formvars(environ).get('document_type', None)
+        response = {"results": []}
+        
+        connection = sqlite3.connect(self.database_path)
+        cursor = connection.cursor()
 
-        #self.cursor.execute('SELECT Url, Title, DocumentType FROM Resources WHERE SearchableText MATCH ? and DocumentType = ?')
-        response = {
-            "results": [
-              {'title': 'A first result %s' %search_term, 
-               'path': '/somewhere/index.html',
-               'document_type': 'Page'},
-              {'title': 'A second result %s' %search_term, 
-               'path': '/nowhere.zip',
-               'document_type': 'File'},
-            ] 
-        }
+        start_response('200 OK', [('content-type', 'application/json')])
+        
+        # check if Resources table exists or exit
+        has_resources_table = cursor.execute("""
+            SELECT name FROM sqlite_master WHERE name='Resources'
+        """).fetchone()        
+        
+        if not has_resources_table: return simplejson.dumps(response)
+        
+        # do query
+        if document_type is not None:
+            results = cursor.execute("""
+                SELECT Url, Title, DocumentType FROM Resources WHERE
+                SearchableText MATCH ?
+            """, (search_term,)).fetchall()
+        else:
+            results = cursor.execute("""
+                SELECT Url, Title, DocumentType FROM Resources WHERE
+                SearchableText MATCH ? and DocumentType = ?
+            """, (search_term, document_type)).fetchall()
+        
+        # return results in json format
+        for i in results:
+            response['results'].append({
+               'title': i[1], 
+               'path': i[0],
+               'document_type': i[2],
+            })
         
         return simplejson.dumps(response)  
 
+
 def app_factory(global_config, **local_conf):
     return OOPSOnlineSearch(local_conf.get('database_path'))
+    
