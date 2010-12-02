26762ab9a4340fcaffe453fe999864815163f1f5
antonio.tirabasso <antonio.tirabasso@d67fe582-f975-982c-faea-898842e22040>
2010-12-02 10:16:43 +0000

    Tagging 0.2

---
+++ packages/oops.searchdumpers/tags/0.2/oops/searchdumpers/dumpers.py
@@ -0,0 +1,130 @@
+import simplejson as json
+
+from oops.staticdump.interfaces import ISearchDataDumper, IExtensionDumper 
+from oops.staticdump import utilities
+from pysqlite2 import dbapi2 as sqlite3
+from zope.interface import implements
+
+
+class OnlineSearchDumper(object):
+    implements(ISearchDataDumper)
+
+    def __init__(self, dumper):
+        self.dumper = dumper
+        self.portal = self.dumper.context
+
+    def dump(self):
+        # get the configured db
+        db_path = '/tmp/search.db'
+        dumper_properties = getattr(self.portal.portal_properties, 
+                                    'dumper_properties', None)
+                                    
+        if dumper_properties is not None:
+            db_path = dumper_properties.getProperty('search_db_path', 
+                                                    '/tmp/search.db')
+        
+        # db connection    
+        connection = sqlite3.connect(db_path)
+        cursor = connection.cursor()
+        
+        # if the Resources table does not exists, create it
+        has_resources_table = cursor.execute("""
+            SELECT name FROM sqlite_master WHERE name='Resources'
+        """).fetchone()
+                
+        if not has_resources_table:
+            try:
+                connection.execute("""
+                    CREATE VIRTUAL TABLE Resources USING fts3(StoreId, Url,
+                    Title, SearchableText, DocumentType)
+                """)
+            except sqlite3.OperationalError:
+                # maybe fts3 extension is not available
+                has_resources_table = False
+
+        if not has_resources_table: return #something wrong here, continue ...
+        
+        # remove all entries of current store_id
+        search_data = self.dumper.search_data
+        store_id = search_data.get('storeid', None)
+
+        if store_id is None: return #something wrong here, continue ...
+        
+        cursor.execute("""
+            DELETE from Resources where StoreId = ? 
+        """, (store_id,))    
+        
+        # load new data and commit
+        row_id = cursor.execute("""
+            SELECT max(rowid) from Resources
+        """).fetchone()[0] or 0
+        
+        for data in search_data.get('contents', []):  
+            row_id += 1  
+            path = convertString(data.get('path', ''))
+            title = convertString(data.get('title', ''))
+            text = convertString(data.get('text', ''))
+            document_type = convertString(data.get('document_type', ''))
+            
+            cursor.execute("""
+                INSERT INTO Resources (rowid, StoreId, Url, Title,
+                SearchableText, DocumentType) VALUES (?, ?, ?, ?, ?, ?)
+            """, (row_id, store_id, path, title, text, document_type))
+
+                
+        connection.commit()           
+    
+def convertString(val):
+    if type(val)==str:
+        val = val.decode('utf-8')
+    return val
+    
+
+class OfflineSearchDumper(object):
+    implements(ISearchDataDumper)
+
+    def __init__(self, dumper):
+        self.dumper = dumper
+        self.portal = self.dumper.context
+
+    def dump(self):
+        self.dumper.save(
+            'searchabletext.json', json.dumps(self.dumper.search_data))
+        self.dumper.manifest_data.add_entry(
+            'searchabletext.json',  utilities.version(self.dumper.context))   
+            
+                 
+class CleanOnlineSearchDumper(object):
+    implements(IExtensionDumper)
+    
+
+    def __init__(self, dumper):
+        self.dumper = dumper
+        self.portal = self.dumper.context
+
+    def dump(self):
+        import pdb;pdb.set_trace()
+        # get the configured db
+        db_path = '/tmp/search.db'
+        dumper_properties = getattr(self.portal.portal_properties, 
+                                    'dumper_properties', None)
+                                    
+        if dumper_properties is not None:
+            db_path = dumper_properties.getProperty('search_db_path', 
+                                                    '/tmp/search.db')
+        
+        # db connection    
+        connection = sqlite3.connect(db_path)
+        cursor = connection.cursor()
+        
+        # if the Resources table exists, drop it
+        has_resources_table = cursor.execute("""
+            SELECT name FROM sqlite_master WHERE name='Resources'
+        """).fetchone()
+        
+        if has_resources_table:
+            connection.execute("""
+                DROP TABLE Resources
+            """)
+        
+        connection.commit()
---
+++ packages/oops.searchdumpers/tags/0.2/setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup, find_packages
+import os
+
+version = '0.2'
+
+setup(name='oops.searchdumpers',
+      version=version,
+      description="Create a static dump of terms site for fulltext search using sqlite",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from
+      # http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Framework :: Plone",
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Reflab s.r.l.',
+      author_email='info@reflab.com',
+      url='http://online-offline-ps.googlecode.com',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['oops'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          #'pysqlite',
+          'oops.staticdump',
+          'simplejson',
+          'zope.interface',
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+
+      [z3c.autoinclude.plugin]
+      target = plone
+      """,
+      )
