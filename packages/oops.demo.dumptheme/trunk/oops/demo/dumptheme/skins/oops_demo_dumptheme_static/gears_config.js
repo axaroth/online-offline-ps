4231d6099341d25e03b3eb8e2b05e7a169fa4d74
riccardo.lemmi@gmail.com <riccardo.lemmi@gmail.com@d67fe582-f975-982c-faea-898842e22040>
2011-01-13 10:10:43 +0000

    added RESOURCES

--- packages/oops.demo.dumptheme/trunk/oops/demo/dumptheme/skins/oops_demo_dumptheme_static/gears_config.js
+++ packages/oops.demo.dumptheme/trunk/oops/demo/dumptheme/skins/oops_demo_dumptheme_static/gears_config.js
@@ -3,6 +3,7 @@
 SITE_NAME = 'OOPS Demo'
 GEARS_MESSAGE = 'Install Gears to enable offline features'
 SITE_URL = $('base').attr('href')
+RESOURCES_URL = SITE_URL + resources_path()
 RETURN_URL = SITE_URL + 'offline.html'
-GEARS_ICON = SITE_URL + 'gears_icon.png'
+GEARS_ICON = RESOURCES_URL + 'gears_icon.png'
 STORE_NAME = 'oops_demo_offline'
