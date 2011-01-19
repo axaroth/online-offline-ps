144101a7148af9fb42a55ced94bc9923a064716a
antonio.tirabasso <antonio.tirabasso@d67fe582-f975-982c-faea-898842e22040>
2011-01-19 10:33:20 +0000

    added toc view

--- packages/oops.demo.mobiletheme/trunk/oops/demo/mobiletheme/browser/pages.py
+++ packages/oops.demo.mobiletheme/trunk/oops/demo/mobiletheme/browser/pages.py
@@ -1,3 +1,4 @@
+from zope.interface import implements, Interface
 from Products.Five import BrowserView
 from Products.CMFCore.utils import getToolByName
 from zope.component import getMultiAdapter
@@ -7,3 +8,41 @@ class SearchView(BrowserView):
   def __init__(self, context, request):
     self.context = context
     self.request = request
+
+    
+class ITocView(Interface):
+  """
+  toc view interface
+  """
+
+class TocView(BrowserView):
+  implements(ITocView)
+
+  def __init__(self, context, request):
+    self.context = context
+    self.request = request
+
+  @property
+  def portal_catalog(self):
+      return getToolByName(self.context, 'portal_catalog')
+
+  @property
+  def portal(self):
+      return getToolByName(self.context, 'portal_url').getPortalObject()
+  
+  def books(self, full_objects=True):
+    """
+     Returns all books
+    """
+    cfilter = {'portal_type': ['Book']}
+    return self.context.getFolderContents(contentFilter = cfilter,
+                                          full_objects = full_objects)
+
+  def chapters(self, book, full_objects=True):
+    """ Returns all chapters and glossaries """
+    cfilter = {'portal_type': ['Chapter', 'Glossary']}
+    return book.getFolderContents(contentFilter = cfilter,
+                                          full_objects = full_objects)
+    
+    
+    
