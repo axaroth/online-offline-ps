30b7eeae8a746702ea0d6cd178c340f377143275
riccardo.lemmi@gmail.com <riccardo.lemmi@gmail.com@d67fe582-f975-982c-faea-898842e22040>
2011-01-21 16:41:49 +0000

    removed deprecated code

--- packages/oops.demo.mobiletheme/trunk/oops/demo/mobiletheme/browser/viewlets.py
+++ packages/oops.demo.mobiletheme/trunk/oops/demo/mobiletheme/browser/viewlets.py
@@ -4,24 +4,24 @@ from Acquisition import aq_inner
 
 class LogoViewlet(common.LogoViewlet):
   render =  ViewPageTemplateFile('templates/logo.pt')
-  
+
   def update(self):
     super(LogoViewlet, self).update()
 
 
 class FooterViewlet(common.ViewletBase):
   render =  ViewPageTemplateFile('templates/footer.pt')
-  
+
   def update(self):
     super(FooterViewlet, self).update()
-    more = self.portal_url + '/more.html'
-    search = self.portal_url + '/search_results.html'
-    toc = self.portal_url + '/toc.html'
+    more = self.site_url + '/more.html'
+    search = self.site_url + '/search_results.html'
+    toc = self.site_url + '/toc.html'
     self.actions = [
-      {'url': self.portal_url, 'title': 'Home', 'id': 'grid'},
+      {'url': self.site_url, 'title': 'Home', 'id': 'grid'},
       {'url': toc, 'title': 'TOC', 'id': 'setup'},
       {'url': more, 'title': 'More', 'id': 'info'},
       {'url': search, 'title': 'Search', 'id': 'search'},
     ]
-    
+
     self.visible = True
