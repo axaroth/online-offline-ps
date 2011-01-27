38c53c44614a9db373d75acce9f7c78fe8e9c6dc
riccardo.lemmi@gmail.com <riccardo.lemmi@gmail.com@d67fe582-f975-982c-faea-898842e22040>
2011-01-27 11:58:46 +0000

    fixed pdf name

--- packages/oops.demo.dumptheme/trunk/oops/demo/dumptheme/browser/viewlets.py
+++ packages/oops.demo.dumptheme/trunk/oops/demo/dumptheme/browser/viewlets.py
@@ -1,12 +1,12 @@
 from plone.app.layout.viewlets import common
+from oops.dumpers.pdf.interfaces import IPDFName
 
-       
 class GlobalSectionsViewlet(common.GlobalSectionsViewlet):
 
     def update(self):
         super(GlobalSectionsViewlet, self).update()
         self.selected_portal_tab = 'index_html'
-        
+
         # customized in order to enable the 'selected' class even w/o request
         # for the static version
         # XXX this is not the best way to fix this problem
@@ -14,16 +14,19 @@ class GlobalSectionsViewlet(common.GlobalSectionsViewlet):
             if tab['id'] == 'index_html': continue
             if self.context.absolute_url().startswith(tab['url']):
                 self.selected_portal_tab = tab['id']
-                break  
+                break
 
 class PdfViewlet(common.ViewletBase):
 
     def update(self):
         super(PdfViewlet, self).update()
-        
+
         self.visible = False
         if self.context.portal_type == 'Book':
             self.visible = True
             self.url = self.context.absolute_url()
-            
-        
+
+    def pdfname(self):
+        return IPDFName(self.context).name()
+
+
