405728827f40e64454d250042cca9e4147f411bc
riccardo.lemmi@gmail.com <riccardo.lemmi@gmail.com@d67fe582-f975-982c-faea-898842e22040>
2011-02-16 14:33:48 +0000

    updated online database code

--- packages/oops.demo.setup/trunk/oops/demo/setup/adapters.py
+++ packages/oops.demo.setup/trunk/oops/demo/setup/adapters.py
@@ -2,6 +2,8 @@ from zope.interface import Interface, implements
 from oops.staticdump.interfaces import IExtensionDumper
 from oops.staticdump import utilities
 
+from logging import getLogger
+LOG = getLogger('oops.demo')
 
 class BaseDumper(object):
 
@@ -14,31 +16,35 @@ class PloneSiteDumper(BaseDumper):
     implements(IExtensionDumper)
 
     def dump(self):
-        print '-- oops.demo plone extension dumper'
+        LOG.info('extension dumper: plone site')
         self.dumper.add_page_html(
                         self.dumper.context,
                         dump_name = 'search_results.html',
                         view='search_results.html')
         self.dumper.manifest_data.remove_entry('search_results.html')
-        self.dumper.manifest_data.add_entry('search_results.html',
-                                            utilities.version(self.dumper.context),
-                                            ignoreQuery=True)
+        self.dumper.manifest_data.add_entry(
+                                      'search_results.html',
+                                      utilities.version(self.dumper.context),
+                                      ignoreQuery=True)
         self.dumper.add_page_html(
                         self.dumper.context,
                         dump_name = 'offline.html',
                         view='offline.html')
-        
+
         try:
             #view available on mobile theme
             self.dumper.add_page_html(
                         self.dumper.context,
                         dump_name = 'toc.html',
                         view='toc_view')
-            
+        except:
+            LOG.info('toc_view BrowserView not found')
+
+        try:
             #view available on mobile theme
             self.dumper.add_page_html(
                         self.dumper.context,
                         dump_name = 'more.html',
                         view='more_view')
         except:
-            print '-- BrowserView not found'
+            LOG.info('more_view BrowserView not found')
