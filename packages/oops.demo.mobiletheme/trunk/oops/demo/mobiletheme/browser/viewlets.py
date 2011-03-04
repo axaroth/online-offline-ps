ecf40186faff1f7519bf400730030d5df4dfc3e5
riccardo.lemmi@gmail.com <riccardo.lemmi@gmail.com@d67fe582-f975-982c-faea-898842e22040>
2011-03-04 10:45:40 +0000

    fixed generation of external urls

--- packages/oops.demo.mobiletheme/trunk/oops/demo/mobiletheme/browser/viewlets.py
+++ packages/oops.demo.mobiletheme/trunk/oops/demo/mobiletheme/browser/viewlets.py
@@ -2,6 +2,8 @@ from plone.app.layout.viewlets import common
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 from Acquisition import aq_inner
 
+from oops.staticdump.utilities import external_url
+
 class LogoViewlet(common.LogoViewlet):
   render =  ViewPageTemplateFile('templates/logo.pt')
 
@@ -10,15 +12,16 @@ class LogoViewlet(common.LogoViewlet):
 
 
 class FooterViewlet(common.ViewletBase):
-  render =  ViewPageTemplateFile('templates/footer.pt')
+  render = ViewPageTemplateFile('templates/footer.pt')
 
   def update(self):
     super(FooterViewlet, self).update()
-    more = self.site_url + '/more.html'
-    search = self.site_url + '/search_results.html'
-    toc = self.site_url + '/toc.html'
+    site_url = external_url(self.context)
+    more = site_url + '/more.html'
+    search = site_url + '/search_results.html'
+    toc = site_url + '/toc.html'
     self.actions = [
-      {'url': self.site_url, 'title': 'Home', 'id': 'grid'},
+      {'url': site_url, 'title': 'Home', 'id': 'grid'},
       {'url': toc, 'title': 'TOC', 'id': 'setup'},
       {'url': more, 'title': 'More', 'id': 'info'},
       {'url': search, 'title': 'Search', 'id': 'search'},
