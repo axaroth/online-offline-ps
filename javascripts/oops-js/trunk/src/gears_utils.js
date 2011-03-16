449e119d57482a2decfc5aeeeb0b6eee81b400fb
riccardo.lemmi@gmail.com <riccardo.lemmi@gmail.com@d67fe582-f975-982c-faea-898842e22040>
2011-03-16 15:01:55 +0000

    copy from old oops-gears

---
+++ javascripts/oops-js/trunk/src/gears_utils.js
@@ -0,0 +1,42 @@
+// DB utils
+/**
+ * For each row in the result set f will be called with the following
+ * parameters: row (map where the column names are the key) and rowIndex.
+ *
+ * @param {Object} db The database object
+ * @param {String} sql The SQL statement to execute
+ * @param {Array} args query params
+ * @param {Function} f Function to call for each row
+ */
+function forEachRow(db, sql, args, f) {
+  var rs = databaseServer.execute(sql, args);
+  try {
+    var rowIndex = 0;
+    var cols = rs.fieldCount();
+    var colNames = [];
+    for (var i = 0; i < cols; i++) {
+      colNames.push(rs.fieldName(i));
+    }
+
+    var rowMap;
+    while (rs.isValidRow()) {
+      rowMap = {};
+      for (var i = 0; i < cols; i++) {
+        rowMap[colNames[i]] = rs.field(i);
+      }
+      f.call(null, rowMap, rowIndex);
+      rs.next();
+      rowIndex++;
+    }
+  } finally {
+    rs.close();
+  }
+}
+
+// Other functions
+function  resources_path() {
+    // extract the name of the resource directory:
+    // <link href="./resources_0/base.css" ... />
+    return  $('link').attr('href').split('/')[1] + '/'
+}
+
---
+++ javascripts/oops-js/trunk/src/oops.search.js
@@ -0,0 +1,130 @@
+/***
+JQUERY PLUGIN oopsSearch
+
+plugin with callback to search oops engine during online and offline mode with Google Gears
+
+This write the result on node selected and with callback is possibile
+work on dom created
+
+params are url paramaters to get from request to interrogate oops system
+sample:
+$('#search-results').oopsSearch({
+  searchabletext : 'SearchableText',
+  documenttype : 'document_type'
+},function(){docTypeValue()});
+
+****/
+
+var SearchableText;
+var DocumentType;
+var searchURL = './search_online'
+var thisObj;
+
+function offlineSearchResults(){
+  console.log("offline");
+  databaseServer = google.gears.factory.create('beta.database');
+  databaseServer.open('search_db');
+  var data = []
+  if (SearchableText!=null && SearchableText!='') {
+    if (DocumentType != null && DocumentType != 'all'){
+        search_query = 'SELECT Url, Title, DocumentType FROM Resources WHERE SearchableText MATCH ? and DocumentType = ?'
+        forEachRow(databaseServer, search_query, [SearchableText, DocumentType], function(item,index){
+            data[index] = {
+                        'Url':item['Url'],
+                        'Title':item['Title'],
+                        'DocumentType':item['DocumentType']
+                        };
+        })
+    } else {
+        search_query = 'SELECT Url, Title, DocumentType FROM Resources WHERE SearchableText MATCH ?'
+        forEachRow(databaseServer, search_query, [SearchableText], function(item,index){
+            data[index] = {
+                        'Url':item['Url'],
+                        'Title':item['Title'],
+                        'DocumentType':item['DocumentType']
+                        };
+        })
+    }
+  }
+  return data;
+}
+
+
+function printResultRow(index, map){
+  var searchResults = thisObj
+  result_link = $('<a />');
+  result_link.attr('href', SITE_URL.slice(0, -1) + map['Url']).html(map['Title']);
+  doctype = map['DocumentType']
+  dt_tag = null;
+  if (doctype != null && doctype != 'n/a' && doctype !=''){
+    dt_tag = $('<div class="discreet">Document Type: </div>');
+    dt_tag.append('<span class="doctype">'+doctype+'</span>');
+  }
+  searchResults.append($('<li />').html(result_link).append(dt_tag));
+
+}
+
+
+
+(function($){
+  
+  $.fn.oopsSearch = function(options,callback){
+    setting = jQuery.extend({
+      searchabletext : 'SearchableText',
+      documenttype : 'document_type'
+      
+    },options);
+    var results = null;
+    SearchableText = $.getURLParam(setting.searchabletext)
+    DocumentType = $.getURLParam(setting.documenttype)
+    thisObj = this
+  
+    //TODO refactor this; oops API?
+    var useGears;
+    if (!window.google || !google.gears){
+      useGears = false;
+    } else{
+      if (!google.gears.factory.hasPermission){
+        useGears = false;
+      } else {
+        useGears = true;  
+      }
+    }
+    
+    if (useGears == true){
+      results = offlineSearchResults();
+      if (results){
+        $.each(results,function(i,item){
+            printResultRow(i,item)
+        })
+        if($.isFunction(callback)){
+            callback.call(thisObj)
+          }
+      }
+    }else{
+      //online search
+      $.getJSON(searchURL,
+        {'SearchTerm':SearchableText,'document_type':DocumentType},
+        function(data){
+          
+          $.each(data.results,function(i,item){
+              data_item = {
+                  'Url':item.path,
+                  'Title':item.title,
+                  'DocumentType':item.document_type
+                  };
+              printResultRow(i,data_item)
+          })
+          
+          if($.isFunction(callback)){
+            callback.call(thisObj)
+          }
+    
+        })
+    }
+    
+    return this;
+  };
+  
+  
+})(jQuery);
