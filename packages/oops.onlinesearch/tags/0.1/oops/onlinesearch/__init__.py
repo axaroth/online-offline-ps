9f52ecdfa3722d9be3619043cf03a6ed635c34ef
riccardo.lemmi@gmail.com <riccardo.lemmi@gmail.com@d67fe582-f975-982c-faea-898842e22040>
2011-05-04 16:23:31 +0000

    bring back fix

--- packages/oops.onlinesearch/tags/0.1/oops/onlinesearch/__init__.py
+++ packages/oops.onlinesearch/tags/0.1/oops/onlinesearch/__init__.py
@@ -8,26 +8,29 @@ class OOPSOnlineSearch(object):
 
     def __init__(self, database_path):
         self.database_path = database_path
-    
+
     def __call__(self, environ, start_response):
         search_term = parse_formvars(environ).get('SearchTerm')
+
         document_type = parse_formvars(environ).get('document_type', None)
+        if document_type in [ 'null', 'all']:
+            document_type = None
         response = {"results": []}
-        
+
         connection = sqlite3.connect(self.database_path)
         cursor = connection.cursor()
 
         start_response('200 OK', [('content-type', 'application/json')])
-        
+
         # check if Resources table exists or exit
         has_resources_table = cursor.execute("""
             SELECT name FROM sqlite_master WHERE name='Resources'
-        """).fetchone()        
-        
+        """).fetchone()
+
         if not has_resources_table: return simplejson.dumps(response)
-        
+
         # do query
-        if document_type is not None:
+        if document_type is None:
             results = cursor.execute("""
                 SELECT Url, Title, DocumentType FROM Resources WHERE
                 SearchableText MATCH ?
@@ -37,18 +40,18 @@ class OOPSOnlineSearch(object):
                 SELECT Url, Title, DocumentType FROM Resources WHERE
                 SearchableText MATCH ? and DocumentType = ?
             """, (search_term, document_type)).fetchall()
-        
+
         # return results in json format
         for i in results:
             response['results'].append({
-               'title': i[1], 
+               'title': i[1],
                'path': i[0],
                'document_type': i[2],
             })
-        
-        return simplejson.dumps(response)  
+
+        return simplejson.dumps(response)
 
 
 def app_factory(global_config, **local_conf):
     return OOPSOnlineSearch(local_conf.get('database_path'))
-    
+
